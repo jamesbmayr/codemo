6f170129e2b29cc552142dd591ad60fd9fe0fc26
jamesbmayr <jamesbmayr@gmail.com>
2017-07-16 16:27:56 -0400

    Add translate-ability to clipPather

--- clipPather/script.js
+++ clipPather/script.js
@@ -1,12 +1,48 @@
 $(document).ready(function() {
 
-	//listeners
+	//textarea listener
 		$(document).on("click","textarea", function() {
 			updatePath(String(this.id).replace("Text",""));
 		});
 
 		$(document).on("keyup","textarea", function() {
 			updatePath(String(this.id).replace("Text",""));
+		});
+
+	//slide button listener
+		$(document).on("click","#adjust", function() {
+			var color = $("#color").val();
+			var direction = $("#direction").val();
+			var distance = $("#distance").val() || false;
+
+			if (distance) {
+				var points = $("#" + color + "Text").val() || false;
+				
+				if (points) {
+					points = points.split(/\s?,\s?/gi);
+					
+					for (i in points) {
+						coordinates = points[i].split(/\s/);
+						var x = Number(coordinates[0].replace(/\%/g,""));
+						var y = Number(coordinates[1].replace(/\%/g,""));
+
+						if (direction == "x") {
+							x = x + Number(distance);
+						}
+						else if (direction == "y") {
+							y = y + Number(distance);
+						}
+
+						var newCoordinates = x + "% " + y + "%";
+						points[i] = newCoordinates;
+					}
+
+					points.join(", ");
+					$("#" + color + "Text").val(points);
+
+					updatePath(color);
+				}
+			}
 		});
 
 	//updatePath
@@ -19,5 +55,6 @@ $(document).ready(function() {
 			$("#" + color + "Path").css("clip-path","polygon(" + path + ")");
 			$("#" + color + "Path").css("webkit-clip-path","polygon(" + path + ")");
 		}
+
 
 });
