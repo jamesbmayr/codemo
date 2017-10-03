dcdc1a0c80480a762fbb209871f0ccc28c126ee8
James Mayr <jamesbmayr@gmail.com>
2017-10-03 17:14:29 -0400

    Add metroMapper

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	/* create page */
-		var demoArray = ["snakeSnacker", "wordShuffler", "quoteTyper", "hueMatcher", "xmlParser","pitchPlayer","dotConnector","numberCruncher","bladesDodger","webDepictor","weatherExplorer","pixelPainter","sphereCollector","codeRunner","shapeAnimator","tickTocker","balloonPopper","robotDirector","colorFlooder","diceRoller","chordAnalyzer","wordFinder","mazeMaker","htmlConverter","tileSlider","wordCounter","clipPather"];
+		var demoArray = ["metroMapper","snakeSnacker", "wordShuffler", "quoteTyper", "hueMatcher", "xmlParser","pitchPlayer","dotConnector","numberCruncher","bladesDodger","webDepictor","weatherExplorer","pixelPainter","sphereCollector","codeRunner","shapeAnimator","tickTocker","balloonPopper","robotDirector","colorFlooder","diceRoller","chordAnalyzer","wordFinder","mazeMaker","htmlConverter","tileSlider","wordCounter","clipPather"];
 			demoArray = demoArray.sort();
 
 		for (i = 0; i < demoArray.length; i++) {
