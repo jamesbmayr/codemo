d6cf5af9c24726c99f10edfe56773c83a305f413
jamesbmayr <jamesbmayr@gmail.com>
2017-11-20 22:05:24 -0500

    Add hexSequencer; remove jQuery, Bootstrap, and Google Fonts

--- resources/script.js
+++ resources/script.js
@@ -1,33 +1,36 @@
-$(document).ready(function(){
+window.onload = function() {
 
 	/* create page */
-		var demoArray = ["pencilPather", "brickBreaker", "blockDescender", "metroMapper", "snakeSnacker", "wordShuffler", "quoteTyper", "hueMatcher", "xmlParser","pitchPlayer","dotConnector","numberCruncher","bladesDodger","webDepictor","weatherExplorer","pixelPainter","sphereCollector","codeRunner","shapeAnimator","tickTocker","balloonPopper","robotDirector","colorFlooder","diceRoller","chordAnalyzer","wordFinder","mazeMaker","htmlConverter","tileSlider","wordCounter","clipPather"];
-			demoArray = demoArray.sort();
+		var demoArray = ["hexSequencer", "pencilPather", "brickBreaker", "blockDescender", "metroMapper", "snakeSnacker", "wordShuffler", "quoteTyper", "hueMatcher", "xmlParser","pitchPlayer","dotConnector","numberCruncher","bladesDodger","webDepictor","weatherExplorer","pixelPainter","sphereCollector","codeRunner","shapeAnimator","tickTocker","balloonPopper","robotDirector","colorFlooder","diceRoller","chordAnalyzer","wordFinder","mazeMaker","htmlConverter","tileSlider","wordCounter","clipPather"]
+			demoArray = demoArray.sort()
 
 		for (i = 0; i < demoArray.length; i++) {
-			$("#demoList").append('<option id="' + demoArray[i] + '" value="' + demoArray[i] + '">' + demoArray[i] + '</option>');
+			var option = document.createElement("option")
+				option.id = demoArray[i]
+				option.value = demoArray[i]
+				option.innerText = demoArray[i]
+			document.getElementById("demoList").appendChild(option)
 		}
 
 	/* load from hash */
 		if (String(location.hash).length > 0) {
-			var selected = String(location.hash).replace("#","");
-			$("#" + selected).attr("selected","true");
-			$("#demo").attr("src", selected + "/index.html");
+			var selected = String(location.hash).replace("#","")
+			document.getElementById(selected).setAttribute("selected","true")
+			document.getElementById("demo").setAttribute("src", selected + "/index.html")
 		}
 		else {
-			var array = $("option").toArray();
-			var random = Math.floor(Math.random() * demoArray.length);
-			var selected = array[random].value;
+			var random = Math.floor(Math.random() * demoArray.length)
+			var selected = demoArray[random]
 			
-			$("#" + selected).attr("selected","true");
-			$("#demo").attr("src", selected + "/index.html");
+			document.getElementById(selected).setAttribute("selected","true")
+			document.getElementById("demo").setAttribute("src", selected + "/index.html")
 		}
 
 	/* listeners */
-		$("#demoList").change(function() {
-			var selected = $(this).val();
-			location.hash = selected;
-			$("#demo").attr("src", selected + "/index.html");
-		});
+		document.getElementById("demoList").addEventListener("change", function(event) {
+			var selected = event.target.value
+			location.hash = selected
+			document.getElementById("demo").setAttribute("src", selected + "/index.html")
+		})
 
-});
+}
