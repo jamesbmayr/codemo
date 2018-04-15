a6875e7e65926adcc46012b7d06e333f1f610ef7
jamesbmayr <jamesbmayr@gmail.com>
2018-04-15 01:54:53 -0400

    only build 1 board to display; toggle fill to show and hide; clean up next, previous, index, count; slice results at 10000

--- wordsearchBuilder/script.js
+++ wordsearchBuilder/script.js
@@ -6,6 +6,7 @@ window.onload = function() {
 			function submitForm() {
 				if (!document.getElementById("submit").getAttribute("loading")) {
 					document.getElementById("submit").setAttribute("loading", true)
+					results = null
 
 					setTimeout(function() {
 						// get diagonal, get & trim words
@@ -23,22 +24,19 @@ window.onload = function() {
 									// get boards & kill workers
 										workers.terminate()
 										workers = undefined
-										var results = event.data
+										results = event.data
+										results = results.slice(0, 10000) || []
 
 									// display count
-										var count = Math.max(0, Math.min(10000, results.length)) || 0
-										document.getElementById("count").innerText = (count ? 1 : 0) + " / " + (count == 10000 ? "10000+" : count) + " results"
+										document.getElementById("index").innerText = (results.length ? 1 : 0)
+										document.getElementById("total").innerText = results.length
 
 									// display boards (fill?)
-										if (count) {
+										if (results.length) {
+											drawBoard(0)
+										}
+										else {
 											document.getElementById("viewer").innerHTML = ""
-											
-											var fill = document.getElementById("fill").checked || false				
-											for (var r in results) {
-												drawBoard(results[r], fill)
-											}
-
-											displayNext()
 										}
 
 									// stop animating the button
@@ -59,98 +57,105 @@ window.onload = function() {
 			}
 
 		/* drawBoard */
-			function drawBoard(board, fill) {
-				if (!board.words.length) { // complete boards only
-					// x and y
-						var minX = 0
-						var maxX = 0
-						var minY = 0
-						var maxY = 0
+			function drawBoard(index) {
+				var board = results[index]
+				var fill  = document.getElementById("fill").checked || false
+				document.getElementById("viewer").innerHTML = ""
+				
+				// x and y
+					var minX = 0
+					var maxX = 0
+					var minY = 0
+					var maxY = 0
 
-						for (var cell in board) {
-							if (cell !== "words") {
-								var x = Number(cell.split(",")[0])
-								var y = Number(cell.split(",")[1])
+					for (var cell in board) {
+						if (cell !== "words") {
+							var x = Number(cell.split(",")[0])
+							var y = Number(cell.split(",")[1])
 
-								if (x < minX) {
-									minX = x
-								}
-								if (x > maxX) {
-									maxX = x
-								}
-								if (y < minY) {
-									minY = y
-								}
-								if (y > maxY) {
-									maxY = y
-								}
+							if (x < minX) {
+								minX = x
+							}
+							if (x > maxX) {
+								maxX = x
+							}
+							if (y < minY) {
+								minY = y
+							}
+							if (y > maxY) {
+								maxY = y
 							}
 						}
+					}
 
-					// build text
-						var text = ""
-						for (var y = minY; y <= maxY; y++) {
-							for (var x = minX; x <= maxX; x++) {
-								text += board[x + "," + y] || (fill ? getRandom() : " ")
-							}
-							text += "\n"
+				// build text
+					var text = ""
+					for (var y = minY; y <= maxY; y++) {
+						for (var x = minX; x <= maxX; x++) {
+							text += (board[x + "," + y] ? ("<span class='letter'>" + board[x + "," + y] + "</span>") : ("<span class='fill'" + (fill ? ' visible' : '') + ">" + getRandom() + "</span>"))
 						}
+						text += "<br>"
+					}
 
-					// display
-						var pre = document.createElement("pre")
-							pre.className = "board"
-							pre.innerText = text
-						document.getElementById("viewer").appendChild(pre)
-				}
+				// display
+					var grid = document.createElement("div")
+						grid.className = "board"
+						grid.id = "_" + index
+						grid.innerHTML = text
+					document.getElementById("viewer").appendChild(grid)
+
+				// set index
+					document.getElementById("index").innerText = index + 1
 			}
 
 		/* displayPrevious */
 			document.getElementById("left").addEventListener("click", displayPrevious)
 			function displayPrevious() {
-				var visible = document.querySelector(".board[visible]") || null
+				// get current
+					var current = Number(document.querySelector(".board").id.replace("_", ""))
 
 				// get previous
-					if (visible) {
-						var previous = visible.previousSibling || visible.parentNode.lastChild || null
-						var text = document.getElementById("count").innerText
-						var total = Number(text.slice(text.indexOf(" / ") + 3).replace(" results", "").replace("+", ""))
-							index = Number(text.slice(0, text.indexOf(" / ")))
-							index = index > 1 ? index - 1 : total
+					if (current == 0) {
+						var previous = results.length - 1
+					}
+					else {
+						var previous = current - 1
 					}
 
-				// set previous or last
-					if (previous) {
-						visible.removeAttribute("visible")
-						previous.setAttribute("visible", true)
-						document.getElementById("count").innerText = index + " / " + total + " results"
-					}
-					else if (document.getElementById("viewer").lastChild) {
-						document.getElementById("viewer").lastChild.setAttribute("visible", true)
-					}
+				// draw previous
+					drawBoard(previous)
 			}
 
 		/* displayNext */
 			document.getElementById("right").addEventListener("click", displayNext)
 			function displayNext() {
-				var visible = document.querySelector(".board[visible]") || null
+				// get current
+					var current = Number(document.querySelector(".board").id.replace("_", ""))
 
 				// get next
-					if (visible) {
-						var next = visible.nextSibling || visible.parentNode.firstChild || null
-						var text = document.getElementById("count").innerText
-						var total = Number(text.slice(text.indexOf(" / ") + 3).replace(" results", "").replace("+", ""))
-							index = Number(text.slice(0, text.indexOf(" / ")))
-							index = (index < total) ? index + 1 : 1
+					if (current == results.length - 1) {
+						var next = 0
+					}
+					else {
+						var next = current + 1
 					}
 
-				// set next or first
-					if (next) {
-						visible.removeAttribute("visible")
-						next.setAttribute("visible", true)
-						document.getElementById("count").innerText = index + " / " + total + " results"
-					}
-					else if (document.getElementById("viewer").firstChild) {
-						document.getElementById("viewer").firstChild.setAttribute("visible", true)
-					}
+				// draw next
+					drawBoard(next)
+			}
+
+		/* toggleFill */
+			document.getElementById("fill").addEventListener("change", toggleFill)
+			function toggleFill() {
+				if (document.getElementById("fill").checked) {
+					Array.from(document.querySelectorAll(".fill")).forEach(function(letter) {
+						letter.setAttribute("visible", true)
+					})
+				}
+				else {
+					Array.from(document.querySelectorAll(".fill")).forEach(function(letter) {
+						letter.removeAttribute("visible")
+					})
+				}
 			}
 }
