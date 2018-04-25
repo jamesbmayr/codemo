0c879b3d9d51b1302f517080a44b277c4af1b60a
James Mayr <jamesbmayr@gmail.com>
2018-04-25 16:34:24 -0400

    adjustable magnetism, friction, gravity, elasticity, maxspeed

--- magnetMaker/script.js
+++ magnetMaker/script.js
@@ -1,6 +1,23 @@
 window.onload = function() {
+
+	/*** globals ***/
+		var state = {
+			paused: false,
+			selected: null,
+			changing: false,
+			changed: false,
+			held: false,
+			moved: false,
+			tips: ["doubleclick to create/remove an orb", "click to change an orb's polarity: red & blue attract", "hold & drag to move an orb", "hold an orb in place for 1 second to stop it", "shift-drag to change orb size", "press space to pause", "magnetMaker"],
+			magnetism: 4,
+			friction: 0,
+			gravity: 0,
+			elasticity: 1,
+			maxspeed: 5
+		}
+		window.state = state
 	
-	/*** create / remove ***/
+	/*** create / remove magnet ***/
 		/* createMagnet */
 			document.getElementById("field").addEventListener("dblclick", createMagnet)
 			function createMagnet(event) {
@@ -31,7 +48,7 @@ window.onload = function() {
 						event.target.appendChild(magnet)
 
 					// tips
-						if (tips.length == 6) {
+						if (state.tips.length == 6) {
 							updateInstructions()
 						}
 				}
@@ -45,82 +62,78 @@ window.onload = function() {
 				}
 			}
 
-	/*** move ***/
+	/*** move magnet ***/
 		/* selectMagnet */
-			var selected = null
 			function selectMagnet(event) {
 				// unselect others
 					unselectMagnet()
 
 				if (event.target.className == "magnet") {
 					// select magnet
-						selected = event.target
-						selected.setAttribute("selected", true)
+						state.selected = event.target
+						state.selected.setAttribute("selected", true)
 
 					// style field
 						document.getElementById("field").setAttribute("dragging", true)
-						held = new Date().getTime()
+						state.held = new Date().getTime()
 
 					// tips
-						if (tips.length == 4) {
+						if (state.tips.length == 4) {
 							updateInstructions()
 						}
 				}
 			}
 
 		/* moveMagnet */
-			var moved = false
-			var held = false
 			document.addEventListener("mousemove", moveMagnet)
 			function moveMagnet(event) {
 				// change mass
-					if (selected && changing) {
+					if (state.selected && state.changing) {
 						changeMass(event)
 					}
 
 				// move & set physics
-					else if (selected) {
-						moved = true
-						selected.style.left   = event.clientX + "px"
-						selected.style.bottom = (window.innerHeight - event.clientY) + "px"
+					else if (state.selected) {
+						state.moved = true
+						state.selected.style.left   = event.clientX + "px"
+						state.selected.style.bottom = (window.innerHeight - event.clientY) + "px"
 
-						selected.setAttribute("x", event.clientX)
-						selected.setAttribute("y", window.innerHeight - event.clientY)
+						state.selected.setAttribute("x", event.clientX)
+						state.selected.setAttribute("y", window.innerHeight - event.clientY)
 
-						held = new Date().getTime()
+						state.held = new Date().getTime()
 					}					
 			}
 
 		/* unselectMagnet */
 			function unselectMagnet(event) {
-				if (selected) {
+				if (state.selected) {
 					// held for 1 second --> v = 0
-						if (held && (new Date().getTime() - held > 1000)) {
-							selected.setAttribute("vx", 0)
-							selected.setAttribute("vy", 0)
+						if (state.held && (new Date().getTime() - state.held > 1000)) {
+							state.selected.setAttribute("vx", 0)
+							state.selected.setAttribute("vy", 0)
 
-							if (tips.length == 3) {
+							if (state.tips.length == 3) {
 								updateInstructions()
 							}
 						}
 
 					// just a click --> flip polarity
-						else if (!moved && !changed) {
-							flipMagnet(selected)
+						else if (!state.moved && !state.changed) {
+							flipMagnet(state.selected)
 						}
 
 					// reset stuff
-						selected.removeAttribute("selected")
-						selected = null
-						moved = false
-						changed = false
-						lastMoved = false
+						state.selected.removeAttribute("selected")
+						state.selected = null
+						state.moved = false
+						state.changed = false
 
 						document.getElementById("field").removeAttribute("dragging")
 				}
 			}
 
-	/*** alter ***/
+	/*** alter magnet ***/
 		/* flipMagnet */
 			function flipMagnet(magnet) {
 				if (magnet.className == "magnet") {
@@ -134,33 +147,32 @@ window.onload = function() {
 						magnet.setAttribute("p", p)
 
 					// tips
-						if (tips.length == 5) {
+						if (state.tips.length == 5) {
 							updateInstructions()
 						}
 				}
 			}
 
 		/* startChanging */
-			var changing = false
-			var changed  = false
-			var paused   = false
 			document.addEventListener("keydown", startChanging)
 			function startChanging(event) {
 				if (event.keyCode == 16) { // SHIFT
 					// set changing
-						changing = true
+						state.changing = true
 				}
 				else if (event.keyCode == 32) { // SPACE
 					// pause / unpause
-						if (paused) {
-							paused = false
+						if (state.paused) {
+							state.paused = false
+							document.getElementById("settings").removeAttribute("visible")
 						}
 						else {
-							paused = true
+							state.paused = true
+							document.getElementById("settings").setAttribute("visible", true)
 						}
 					
 					// tips
-						if (tips.length == 1) {
+						if (state.tips.length == 1) {
 							updateInstructions()
 						}
 				}
@@ -171,15 +183,15 @@ window.onload = function() {
 			function stopChanging(event) {
 				if (event.keyCode == 16) { // SHIFT
 					// unset changing
-						changing = false
+						state.changing = false
 				}
 			}
 
 		/* changeMass */
 			function changeMass(event) {
 				// get position
-					var x = Number(selected.getAttribute("x"))
-					var y = Number(selected.getAttribute("y"))
+					var x = Number(state.selected.getAttribute("x"))
+					var y = Number(state.selected.getAttribute("y"))
 
 				// calculate distance to cursor
 					var cx = event.clientX
@@ -188,13 +200,13 @@ window.onload = function() {
 					var d = Math.abs(getScalar(getDifference(cx, x), getDifference(cy, y)))
 
 				// update mass and size
-					selected.setAttribute("m", d)
-					selected.style.height = d * 2 + "px"
-					selected.style.width  = d * 2 + "px"
-					changed = true
+					state.selected.setAttribute("m", d)
+					state.selected.style.height = d * 2 + "px"
+					state.selected.style.width  = d * 2 + "px"
+					state.changed = true
 
 				// tips
-					if (tips.length == 2) {
+					if (state.tips.length == 2) {
 						updateInstructions()
 					}				
 			}
@@ -242,17 +254,22 @@ window.onload = function() {
 	/*** magnet interaction ***/
 		/* magnetLoop */
 			var magnetLoop = setInterval(function() {
-				if (!paused) {
+				if (!state.paused) {
 					// get magnets and empty forces object
 						var magnets = Array.from(document.querySelectorAll(".magnet")) || []
 						var forces = resetForces(magnets)
 						var collisions = []
 
-					// calculate positions
+					// calculate magnetic force
 						magnets.forEach(function (magnet) {
 							var data = updateForce(magnet, magnets, forces, collisions)
 							forces = data[0]
 							collisions = data[1]
+						})
+
+					// calculate gravity
+						magnets.forEach(function (magnet) {
+							forces = updateGravity(magnet, forces)
 						})
 
 					// update velocities for each collision
@@ -319,7 +336,7 @@ window.onload = function() {
 						// magnetic force
 							if (p1 !== 0 && p2 !== 0) {
 								// calculate force
-									var f = 4 * (m1 * m2) / Math.pow(d, 2)
+									var f = state.magnetism * (m1 * m2) / Math.pow(d, 2)
 								
 								// flip for same polarities
 									if (p1 == p2) {
@@ -345,6 +362,13 @@ window.onload = function() {
 				return [forces, collisions]
 			}
 
+		/* updateGravity */
+			function updateGravity(magnet, forces) {
+				forces[magnet.id].y -= state.gravity
+
+				return forces
+			}
+
 		/* updateVelocity */
 			function updateVelocity(magnet, forces) {
 				// get forces
@@ -360,9 +384,13 @@ window.onload = function() {
 					var ax = fx / m
 					var ay = fy / m
 
+				// calculate friction
+					vx = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, (vx + ax) * (1 - Math.pow(state.friction, 2))))
+					vy = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, (vy + ay) * (1 - Math.pow(state.friction, 2))))
+
 				// calculate new velocity
-					magnet.setAttribute("vx", vx + ax)
-					magnet.setAttribute("vy", vy + ay)
+					magnet.setAttribute("vx", vx)
+					magnet.setAttribute("vy", vy)
 			}
 
 		/* updateCollision */
@@ -386,8 +414,8 @@ window.onload = function() {
 
 				// acceleration
 					var angle = Math.atan2(dy, dx)
-					var ax    = (Math.cos(angle) * m) + (x1 - x2)
-					var ay    = (Math.sin(angle) * m) + (y1 - y2)
+					var ax    = ( (Math.cos(angle) * m) + (x1 - x2) ) * state.elasticity
+					var ay    = ( (Math.sin(angle) * m) + (y1 - y2) ) * state.elasticity
 
 				// vx & vy (initial)
 					var vx1 = Number(magnet1.getAttribute("vx"))
@@ -396,21 +424,27 @@ window.onload = function() {
 					var vy2 = Number(magnet2.getAttribute("vy"))
 
 				// vx & vy (final)
-					magnet1.setAttribute("vx", vx1 - (ax * m2 / m))
-					magnet1.setAttribute("vy", vy1 - (ay * m2 / m))
-					magnet2.setAttribute("vx", vx2 + (ax * m1 / m))
-					magnet2.setAttribute("vy", vy2 + (ay * m1 / m))
+					vx1 = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, vx1 - (ax * m2 / m)))
+					vy1 = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, vy1 - (ay * m2 / m)))
+					vx2 = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, vx2 + (ax * m1 / m)))
+					vy2 = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, vy2 + (ay * m1 / m)))
+
+				// vx & vy (final)
+					magnet1.setAttribute("vx", vx1)
+					magnet1.setAttribute("vy", vy1)
+					magnet2.setAttribute("vx", vx2)
+					magnet2.setAttribute("vy", vy2)
 			}
 
 		/* updatePosition */
 			function updatePosition(magnet) {
-				if (!selected || selected.id !== magnet.id) {
+				if (!state.selected || state.selected.id !== magnet.id) {
 					// get position & velocity
 						var m = Number(magnet.getAttribute("m"))
 						var x = Number(magnet.getAttribute("x"))
 						var y = Number(magnet.getAttribute("y"))
-						var vx = Math.min(10, Math.max(-10, Number(magnet.getAttribute("vx"))))
-						var vy = Math.min(10, Math.max(-10, Number(magnet.getAttribute("vy"))))
+						var vx = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, Number(magnet.getAttribute("vx"))))
+						var vy = Math.min(state.maxspeed, Math.max(state.maxspeed * -1, Number(magnet.getAttribute("vy"))))
 
 					// update x position or velocity
 						if (x + vx - m <= 0) { // left
@@ -446,16 +480,26 @@ window.onload = function() {
 				}
 			}
 
-	/*** instructions ***/
+	/*** instructions / settings ***/
 		/* updateInstructions */
-			var tips = ["doubleclick to create/remove an orb", "click to change an orb's polarity: red & blue attract", "hold & drag to move an orb", "hold an orb in place for 1 second to stop it", "shift-drag to change orb size", "press space to pause", "magnetMaker"]
 			updateInstructions()
 			function updateInstructions() {
 				// update instructions element
 					var instructions = document.getElementById("instructions")
-						instructions.innerHTML = tips[0] || ""
+						instructions.innerHTML = state.tips[0] || ""
 
 				// remove first tip
-					tips.shift()
+					state.tips.shift()
+			}
+
+		/* updateSettings */
+			Array.from(document.querySelectorAll("#settings input")).forEach(function(input) {
+				input.addEventListener("change", updateSettings)
+			})
+			function updateSettings(event) {
+				if (event.target.tagName.toLowerCase() == "input") {
+					var input = event.target.id.replace("-slider", "")
+					state[input] = Number(event.target.value)
+				}
 			}
 }
