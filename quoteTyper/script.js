82953945e5b1dbaec3dd67f4ed17dda288c3b0a0
James Mayr <jamesbmayr@gmail.com>
2022-07-19 22:43:16 -0400

    update quoteTyper - API fix

--- quoteTyper/script.js
+++ quoteTyper/script.js
@@ -1,90 +1,108 @@
-window.onload = function() {
+/* globals */
+	var STATE = {
+		api: "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
+		startTime: null,
+		quotes: [],
+		quote: null
+	}
 
-	/* triggers */
-		if ((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i).test(navigator.userAgent)) {
-			var on = { click: "touchstart", mousedown: "touchstart", mousemove: "touchmove", mouseup: "touchend" }
-		}
-		else {
-			var on = { click:      "click", mousedown:  "mousedown", mousemove: "mousemove", mouseup:  "mouseup" }
-		}
+/* elements */
+	var ELEMENTS = {
+		go: document.querySelector("#go"),
+		speed: document.querySelector("#speed"),
+		quote: document.querySelector("#quote"),
+		input: document.querySelector("#input")
+	}
 
-	/* apiRequest */
-		document.querySelectorAll("#go")[0].addEventListener(on.click, apiRequest)
-		function apiRequest() {
-			var request = new XMLHttpRequest()
-
+/* requestQuotes */
+	function requestQuotes() {
+		var request = new XMLHttpRequest()
 			request.onreadystatechange = function() {
 				if (request.readyState == XMLHttpRequest.DONE) {
 					if (request.status == 200) {
 						try {
-							console.log("Quote fetched.")
-							eval(request.responseText)
+							receiveQuotes(JSON.parse(request.responseText))
 						}
-						catch (error) {
-							console.log(error)
-						}
+						catch (error) { console.log(error) }
 					}
 					else {
 						console.log("Unable to fetch a quote: " + request.status)
 					}
 				}
 			}
-
-			request.open("GET", "//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&_jsonp=displayQuote&callback=?", true)
+			request.open("GET", STATE.api + "&randomnumber=" + Math.floor((Math.random() * 10e6)), true)
 			request.send()
+	}
+
+/* receiveQuotes */
+	function receiveQuotes(quotes) {
+		for (var i in quotes) {
+			STATE.quotes.push(quotes[i])
+		}
+		displayQuote()
+	}
+
+/* displayQuote */
+	displayQuote()
+	ELEMENTS.go.addEventListener("click", displayQuote)
+	function displayQuote() {
+		if (!STATE.quotes.length) {
+			requestQuotes()
+			return
 		}
 
-	/* displayQuote */
-		function displayQuote(objects) {
-			var object = objects[Math.floor(Math.random() * objects.length)]
-			var author = object.title.trim()
-				author = author.replace(/\&\#8217\;|\&\#8216\;|\&\#8242\;/gi,"\'").replace(/\&\#8220\;|\&\#8221\;|\&\#8243\;/gi,"\"").replace(/\&\#8211\;|\&\#8212\;/gi, "-").replace(/\\u2026/gi,"...").replace(/(<([^>]+)>)/ig,"").trim()
-			var phrase = object.content.replace(/\<p\>|\<\/p\>|\<\\\/p\>|\n|\\n/gi,"").trim()
-				phrase = phrase.replace(/\&\#8217\;|\&\#8216\;|\&\#8242\;/gi,"\'").replace(/\&\#8220\;|\&\#8221\;|\&\#8243\;/gi,"\"").replace(/\&\#8211\;|\&\#8212\;/gi, "-").replace(/\\u2026/gi,"...").replace(/(<([^>]+)>)/ig,"").trim()
+		var index = Math.floor(Math.random() * STATE.quotes.length)
+		var object = STATE.quotes[index]
+		var author = cleanString(object.title.rendered.trim())
+		var phrase = cleanString(object.content.rendered.replace(/\<p\>|\<\/p\>|\<\\\/p\>|\n|\\n/gi,"").trim())
 
-			window.startTime = new Date().getTime()
-			window.quote = phrase + " -" + author
+		STATE.startTime = new Date().getTime()
+		STATE.quote = phrase + " -" + author
 
-			document.querySelectorAll("#speed")[0].textContent = "???"
-			document.querySelectorAll("#quote")[0].className = ""
-			document.querySelectorAll("#quote")[0].innerHTML = "<span>" + window.quote + "</span>"
-			document.querySelectorAll("#input")[0].value = ""
-			document.querySelectorAll("#input")[0].disabled = false
-			document.querySelectorAll("#input")[0].focus()
-		}
+		ELEMENTS.speed.textContent = "???"
+		ELEMENTS.quote.className = ""
+		ELEMENTS.quote.innerHTML = "<span>" + STATE.quote + "</span>"
+		ELEMENTS.input.value = ""
+		ELEMENTS.input.disabled = false
+		ELEMENTS.input.focus()
 
+		STATE.quotes.splice(index, 1)
+	}
 
-	/* checkWords */
-		document.addEventListener("keyup", checkWords)
-		function checkWords(event) {
-			if (window.quote) {
-				var keyCode = event.which
-				var text = document.querySelectorAll("#input")[0].value || ""
-				var endTime = new Date().getTime()
-				
-				var reflection = ""
-				for (var l = 0; l < window.quote.length; l++) {
-					if (text[l] && window.quote[l] == text[l]) {
-						reflection += "<span class='correct'>" + window.quote[l] + "</span>"
-					}
-					else if (text[l]) {
-						reflection += "<span class='incorrect'>" + window.quote[l] + "</span>"
-					}
-					else {
-						reflection += "<span>" + window.quote[l] + "</span>"	
-					}
+/* cleanString */
+	function cleanString(str) {
+		return str.replace(/\“/g, '"').replace(/\”/g, '"').replace(/\’/g, "'").replace(/\‘/g, "'").replace(/\—/g, "-").replace(/\&\#8217\;|\&\#8216\;|\&\#8242\;/gi,"\'").replace(/\&\#8220\;|\&\#8221\;|\&\#8243\;/gi,"\"").replace(/\&\#8211\;|\&\#8212\;/gi, "-").replace(/\\u2026/gi,"...").replace(/(<([^>]+)>)/ig,"").trim()
+	}
+
+/* checkWords */
+	ELEMENTS.input.addEventListener("keyup", checkWords)
+	function checkWords(event) {
+		if (STATE.quote) {
+			var keyCode = event.which
+			var text = ELEMENTS.input.value || ""
+			var endTime = new Date().getTime()
+			
+			var reflection = ""
+			for (var l = 0; l < STATE.quote.length; l++) {
+				if (text[l] && STATE.quote[l] == text[l]) {
+					reflection += "<span class='correct'>" + STATE.quote[l] + "</span>"
 				}
-				document.querySelectorAll("#quote")[0].innerHTML = reflection
-
-				if (text == window.quote) {
-					document.querySelectorAll("#quote")[0].className = "complete"
-					document.querySelectorAll("#input")[0].disabled = true
-					document.querySelectorAll("#speed")[0].textContent = Number(window.quote.split(/\s/gi).length / ((endTime - window.startTime) / 1000 / 60)).toFixed(2)
-
-					window.startTime = null
-					window.quote = null
+				else if (text[l]) {
+					reflection += "<span class='incorrect'>" + STATE.quote[l] + "</span>"
+				}
+				else {
+					reflection += "<span>" + STATE.quote[l] + "</span>"	
 				}
 			}
-		}
+			ELEMENTS.quote.innerHTML = reflection
 
-}
+			if (text == STATE.quote) {
+				ELEMENTS.quote.className = "complete"
+				ELEMENTS.input.disabled = true
+				ELEMENTS.speed.textContent = Number(STATE.quote.split(/\s/gi).length / ((endTime - STATE.startTime) / 1000 / 60)).toFixed(2)
+
+				STATE.startTime = null
+				STATE.quote = null
+			}
+		}
+	}
