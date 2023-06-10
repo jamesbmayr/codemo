ac77b4f8554bec5dd4546c449fd04a0e6a135818
James Mayr <jamesbmayr@gmail.com>
2023-06-10 00:30:58 -0400

    fix tiprounder for mobile, add more options

--- tiprounder/script.js
+++ tiprounder/script.js
@@ -1,40 +1,24 @@
-window.onload = function() {
-	
-	/* listeners */
-		document.getElementById("cost").addEventListener("change", calculateTip)
-		document.getElementById("percent").addEventListener("change", calculateTip)
-		Array.from(document.querySelectorAll("input[name='coin']")).forEach(function(coin) {
-			coin.addEventListener("change", selectCoin)
-		})
+/* listeners */
+	document.getElementById("cost").addEventListener("input", calculateTip)
+	document.getElementById("percent").addEventListener("input", calculateTip)
+	Array.from(document.querySelectorAll("input[name='denomination']")).forEach(function(denomination) {
+		denomination.addEventListener("input", calculateTip)
+	})
 
-	/* selectCoin */
-		function selectCoin(event) {
-			// unselect all coins
-				Array.from(document.querySelectorAll("[name='coin']")).forEach(function(coin) {
-					coin.parentNode.removeAttribute("selected")
-				})
+/* calculateTip */
+	function calculateTip() {
+		// get data
+			var cost = Math.floor(Number(document.getElementById("cost").value) * 100) / 100
+			var percent = Number(document.getElementById("percent").value) / 100
+			var denomination = Number(document.querySelector("input[name='denomination']:checked").value)
 
-			// select this coin
-				event.target.parentNode.setAttribute("selected", true)
-				calculateTip()
-		}
+		// math (raw --> rounded --> difference)
+			var total = cost * (1 + percent)
+				total = Math.ceil(total / denomination) * denomination
+				total = Math.ceil(total * 100) / 100
+			var tip = total - cost
 
-	/* calculateTip */
-		function calculateTip() {
-			// get data
-				var cost = Math.floor(Number(document.getElementById("cost").value) * 100) / 100
-				var percent = Number(document.getElementById("percent").value) / 100
-				var coin = Number(document.querySelector("label[selected]").querySelector("input[name='coin']").value)
-
-			// math (raw --> rounded --> difference)
-				var total = cost * (1 + percent)
-					total = Math.round(total / coin) * coin
-					total = Math.round(total * 100) / 100
-				var tip = total - cost
-
-			// display
-				document.getElementById("output-tip").value = tip.toFixed(2)
-				document.getElementById("output-total").value = total.toFixed(2)
-			
-		}
-}
+		// display
+			document.getElementById("output-tip").value = tip.toFixed(2)
+			document.getElementById("output-total").value = total.toFixed(2)	
+	}
