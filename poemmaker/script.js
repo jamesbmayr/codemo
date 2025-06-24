334333a936c0926c784d8915e71439ea41e5de94
James Mayr <jamesbmayr@gmail.com>
2025-06-23 20:29:36 -0400

    add pushkin sonnet

--- poemmaker/script.js
+++ poemmaker/script.js
@@ -107,6 +107,7 @@
 				"ovillejo": 				["s8a","s4a","/","s8b","s4b","/","s8c","s4c","/","s8c","s8d","s8d","r-1","r-4","r-7"],
 				"pantoum": 					["i4a","a3b","i4a","a3b","/","r-1","i4c","r-3","i4c","/","r-6","a3d","r-8","a3d","/","r-11","r-2","r-13","r-0"],
 				"petrarchan sonnet": 		["i5a","i5b","i5b","i5a","i5a","i5b","i5b","i5a","/","i5c","i5d","i5e","i5c","i5d","i5e"],
+				"pushkin sonnet": 			["i4a","i4b","i4a","i4b","i4c","i4c","i4d","i4d","/","i4e","i4f","i4f","i4e","i4g","i4g"],
 				"quatern": 					["s8a","s8b","s8c","s8d","/","s8e","r-0","s8f","s8g","/","s8h","s8i","r-0","s8j","/","s8k","s8l","s8m","r-0"],
 				"quatrain": 				["i4a","i4a","i4b","i4b"],
 				"rhyme royal": 				["i5a","i5b","i5a","/","i5b","i5b","/","i5c","i5c"],
@@ -131,7 +132,7 @@
 				"zejel": 					["i4a","i4a","i4a","/","i4b","i4b","i4b","i4a","/","i4c","i4c","i4c","i4a","/","i4d","i4d","i4d","i4a"],
 			},
 			formGroups: {
-				"iambic": 		["iambic couplet", "quatrain", "heroic stanza", "cinquain", "rhyme royal", "common hymn", "blank verse", "ottava rima", "terza rima", "dizain", "minute", "rondel", "madrigal", "petrarchan sonnet", "shakespearean sonnet", "zejel", "interlocking rubaiyat", "villanelle", "ballade"],
+				"iambic": 		["iambic couplet", "quatrain", "heroic stanza", "cinquain", "rhyme royal", "common hymn", "blank verse", "ottava rima", "terza rima", "dizain", "minute", "rondel", "madrigal", "petrarchan sonnet", "shakespearean sonnet", "pushkin sonnet", "zejel", "interlocking rubaiyat", "villanelle", "ballade"],
 				"anapestic": 	["anapestic couplet", "limerick", "strambotto", "seussian", "anapestic blank verse", "anapestic sonnet", "anaduo", "galloping denturn", "blood quill"],
 				"syllabic": 	["free verse", "haiku", "senryu", "zappai", "kimo", "kouta", "dodoitsu", "ballad", "endecha", "gwawdodyn", "tanka", "waka", "bob and wheel", "flamenca", "rime couee", "shadorma", "tripadi", "rondelet", "seguidilla", "chant", "imayo", "tricube", "nonet", "lai", "treochair", "decima", "ovillejo", "haiku sonnet", "kyrielle", "quatern", "pantoum", "12-bar blues", "sports taunt"]
 			},
